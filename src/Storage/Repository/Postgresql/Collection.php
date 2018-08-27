02a1047c00a5f752e9ac5ffc173d03ae00753325
Piotr Marczyszyn <pm@miinto.com>
2018-08-27 14:58:07 +0200

    - implemented postgresql generic collection repository

--- src/Storage/Repository/Postgresql/Collection.php
+++ src/Storage/Repository/Postgresql/Collection.php
@@ -20,7 +20,7 @@ class Collection implements \Maleficarum\Storage\Repository\CollectionInterface
      * @see \Maleficarum\Storage\Repository\CollectionInterface.populate()
      */
     public function populate(\Maleficarum\Data\Collection\Persistable\AbstractCollection $collection, array $parameters = []): \Maleficarum\Storage\Repository\CollectionInterface {
-        // connect to shard if necessary
+        // fetch the shard object
         $shard = $this->getStorage()->fetchShard('Postgresql', $collection->getShardRoute());
         
         // execute parameter checks
@@ -40,8 +40,34 @@ class Collection implements \Maleficarum\Storage\Repository\CollectionInterface
 
         // blanket SQL statement
         $query = $this->populate_blanketSQL($query);
+
+        // attach grouping info
+        (array_key_exists('__count', $parameters) || array_key_exists('__sum', $parameters)) and $query = $this->populate_grouping($query, $dto);
+
+        // attach sorting info
+        array_key_exists('__sorting', $parameters) and $query = $this->populate_sorting($query, $dto);
+
+        // attach subset info
+        array_key_exists('__subset', $parameters) and $query = $this->populate_subset($query, $dto);
+
+        // attach lock directive (caution - this can cause deadlocks when used incorrectly)
+        array_key_exists('__lock', $parameters) and $query = $this->populate_lock($query, $dto);
         
-        var_dump($query);exit;
+        // establish the connection if necessary
+        $shard->isConnected() or $shard->connect();
+        
+        // fetch data from the persistence layer
+        $statement = $shard->prepare($query);
+        foreach ($dto->params as $param => $value) $statement->bindValue($param, $value);
+        $statement->execute();
+        
+        $result = $statement->fetchAll();
+        
+        // apply retrieval transformations
+        $result = $this->transformForRetrieval($result);
+        
+        // transfer data to the collection object
+        $collection->populate($result);
         
         return $this;
     }
@@ -50,6 +76,54 @@ class Collection implements \Maleficarum\Storage\Repository\CollectionInterface
      * @see \Maleficarum\Storage\Repository\CollectionInterface.createAll()
      */
     public function createAll(\Maleficarum\Data\Collection\Persistable\AbstractCollection $collection): \Maleficarum\Storage\Repository\CollectionInterface {
+        // fetch the shard object
+        $shard = $this->getStorage()->fetchShard('Postgresql', $collection->getShardRoute());
+        
+        // transform data for persistence
+        $data = $this->transformForPersistence($collection->toArray());
+        
+        // setup containers
+        $sets = [];
+        $params = [];
+        
+        // establish data containers
+        foreach ($data as $key => &$val) {
+            // remove id column if present
+            if (array_key_exists($collection->getModelPrefix().'Id', $val)) unset($val[$collection->getModelPrefix().'Id']);
+            
+            $result = [];
+            array_walk($val, function ($local_value, $local_key) use (&$result, $key) {
+                $result[':' . $local_key . '_token_' . $key] = $local_value;
+            });
+
+            // append sets
+            $sets[] = "(" . implode(', ', array_keys($result)) . ")";
+
+            // append bind params
+            $params = array_merge($params, $result);
+        }
+        
+        // generate SQL query
+        $sql = 'INSERT INTO "' . $collection->getStorageGroup() . '" ("' . implode('", "', array_keys($data[0])) . '") VALUES ';
+        $sql .= implode(', ', $sets);
+        $sql .= ' RETURNING *;';
+
+        // establish the connection if necessary
+        $shard->isConnected() or $shard->connect();
+        
+        // send data to the persistence layer
+        $statement = $shard->prepare($sql);
+        foreach ($params as $k => $v) $statement->bindValue($k, $v);
+        $statement->execute();
+        
+        $result = $statement->fetchAll();
+
+        // apply retrieval transformations
+        $result = $this->transformForRetrieval($result);
+
+        // transfer data to the collection object
+        $collection->populate($result);
+        
         return $this;
     }
 
@@ -57,6 +131,26 @@ class Collection implements \Maleficarum\Storage\Repository\CollectionInterface
      * @see \Maleficarum\Storage\Repository\CollectionInterface.deleteAll()
      */
     public function deleteAll(\Maleficarum\Data\Collection\Persistable\AbstractCollection $collection): \Maleficarum\Storage\Repository\CollectionInterface {
+        // fetch the shard object
+        $shard = $this->getStorage()->fetchShard('Postgresql', $collection->getShardRoute());
+        
+        // extract entity ids
+        $ids = [];
+        foreach ($collection->toArray() as $key => $col_element) {
+            isset($col_element[$collection->getModelPrefix().'Id']) and $ids[':_id_token_'.$key] = $col_element[$collection->getModelPrefix().'Id']; 
+        }
+        
+        // build the sql query
+        $temp = [];
+        $sql = 'DELETE FROM "' . $collection->getStorageGroup() . '" WHERE "'.$collection->getModelPrefix().'Id" IN (';
+        foreach ($ids as $key => $val) $temp[] = $key;
+        $sql .= implode(', ', $temp).')';
+        
+        // remove data from the persistence layer
+        $statement = $shard->prepare($sql);
+        foreach ($ids as $k => $v) $statement->bindValue($k, $v);
+        $statement->execute();
+        
         return $this;
     }
 
@@ -64,14 +158,14 @@ class Collection implements \Maleficarum\Storage\Repository\CollectionInterface
      * @see \Maleficarum\Storage\Repository\CollectionInterface.transformForRetrieval()
      */
     public function transformForRetrieval(array $data): array {
-        return [];
+        return $data;
     }
     
     /**
      * @see \Maleficarum\Storage\Repository\CollectionInterface.Repository()
      */
     public function transformForPersistence(array $data): array {
-        return [];
+        return $data;
     }
 
     /**
@@ -201,9 +295,7 @@ class Collection implements \Maleficarum\Storage\Repository\CollectionInterface
     protected function populate_attachFilter(string $query, \stdClass $dto): string {
         foreach ($dto->data as $key => $data) {
             // skip any special tokens
-            if (in_array($key, self::$specialTokens)) {
-                continue;
-            }
+            if ($key[0] === '_' && $key[1]) continue;
 
             // validate token data
             is_array($data) && count($data) or $this->respondToInvalidArgument('Incorrect filter param provided [' . $key . '], non-empty array expected. \%s::populate()');
@@ -236,6 +328,130 @@ class Collection implements \Maleficarum\Storage\Repository\CollectionInterface
     protected function populate_blanketSQL(string $query): string {
         return $query . '1=1 ';
     }
+
+    /**
+     * Attach grouping section to the query.
+     * 
+     * @param string $query
+     * @param \stdClass $dto
+     * @return string
+     */
+    protected function populate_grouping(string $query, \stdClass $dto): string {
+        if (array_key_exists('__count', $dto->data) && is_array($dto->data['__count']) && count($dto->data['__count'])) {
+            $query .= 'GROUP BY "' . implode('", "', $dto->data['__count']) . '" ';
+        }
+
+        if (array_key_exists('__sum', $dto->data) && is_array($dto->data['__sum']) && count($dto->data['__sum'])) {
+            $query .= 'GROUP BY "' . implode('", "', $dto->data['__sum']) . '" ';
+        }
+
+        return $query;
+    }
+
+    /**
+     * Attach sorting section to the query.
+     * 
+     * @param string $query
+     * @param \stdClass $dto
+     * @return string
+     */
+    protected function populate_sorting(string $query, \stdClass $dto): string {
+        $query .= 'ORDER BY ';
+        $fields = [];
+        foreach ($dto->data['__sorting'] as $val) {
+            $fields[] = "\"$val[0]\" $val[1]";
+        }
+        $query .= implode(', ', $fields) . ' ';
+
+        return $query;
+    }
+    
+    /**
+     * Attach subset section to the query.
+     * 
+     * @param string $query
+     * @param \stdClass $dto
+     * @return string
+     */
+    protected function populate_subset(string $query, \stdClass $dto): string {
+        $query .= 'LIMIT :limit OFFSET :offset ';
+        $dto->params[':limit'] = $dto->data['__subset']['limit'];
+        $dto->params[':offset'] = $dto->data['__subset']['offset'];
+
+        return $query;
+    }
+
+    /**
+     * Attach locking section to the query.
+     * 
+     * @param string $query
+     * @param \stdClass $dto
+     * @return string
+     */
+    protected function populate_lock(string $query, \stdClass $dto): string {
+        array_key_exists('__lock', $dto->data) and $query .= 'FOR UPDATE ';
+
+        return $query;
+    }
     
     /* ------------------------------------ Populate Methods END --------------------------------------- */
+
+    /* ------------------------------------ Helper methods START --------------------------------------- */
+
+    /**
+     * Parse provided key param into a set of filtering data.
+     *
+     * @param string $key
+     * @return array
+     */
+    private function parseFilter(string $key): array {
+        $result = ['column' => '"' . $key . '"', 'operator' => 'IN', 'prefix' => ':' . $key . '_', 'value_prefix' => '', 'value_suffix' => ''];
+
+        // attempt to recover filters
+        $data = explode('/', $key);
+
+        // filters detected
+        if (count($data) > 1) {
+            // fetch filters
+            $filter = array_shift($data);
+
+            // establish a new basic result structure
+            $result = ['column' => '"' . $data[0] . '"', 'operator' => $result['operator'], 'prefix' => ':' . $data[0] . '_', 'value_prefix' => $result['value_prefix'], 'value_suffix' => $result['value_suffix']];
+
+            // apply filters
+            for ($index = 0; $index < mb_strlen($filter); $index++) {
+                // exclude filter
+                $filter[$index] === '~' and $result = [
+                    'column' => $result['column'],
+                    'operator' => 'NOT IN',
+                    'prefix' => $result['prefix'] . 'exclude_',
+                    'value_prefix' => $result['value_prefix'],
+                    'value_suffix' => $result['value_suffix'],
+                ];
+
+                // case-insensitive filter
+                $filter[$index] === 'i' and $result = [
+                    'column' => 'LOWER(' . $result['column'] . ')',
+                    'operator' => $result['operator'],
+                    'prefix' => $result['prefix'] . 'case_insensitive_',
+                    'value_prefix' => 'LOWER(' . $result['value_prefix'],
+                    'value_suffix' => $result['value_suffix'] . ')',
+                ];
+            }
+        }
+
+        return $result;
+    }
+    
+    /**
+     * This method is a code style helper - it will simply throw an \InvalidArgumentException
+     *
+     * @param string $msg
+     * @return void
+     */
+    protected function respondToInvalidArgument(string $msg) {
+        throw new \InvalidArgumentException(sprintf($msg, static::class));
+    }
+
+    /* ------------------------------------ Helper methods END ----------------------------------------- */
 }
